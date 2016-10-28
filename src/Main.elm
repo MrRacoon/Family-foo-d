2a7ce02494736b302af2127f8a030acaf7141210
Erik Sutherland <erik.misu@gmail.com>
2016-10-27 23:47:27 -0700

    randomize questions

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,16 @@
 module Main exposing (..)
 
-import Random
+import Platform.Cmd exposing (Cmd)
+import Platform.Sub exposing (Sub)
+import Cmd.Extra exposing (message)
+
+import String exposing (toLower, toUpper)
 import List exposing (head, drop, length)
 
-import Html exposing (Html, div, button, text, a)
+import Html exposing (..)
 import Html.Lazy exposing (lazy)
-import Html.App exposing (beginnerProgram)
+import Html.App exposing (program)
+import Html.Events exposing (..)
 
 import Styling exposing (classMain)
 
@@ -14,12 +19,20 @@ import Intro.Intro as Intro
 import Questions.Component as Question
 import Questions.All exposing (everyQuestion)
 
+import Random
+
+questionGen = Random.int 0
+  <| (\n -> n - 1)
+  <| length everyQuestion
+
+
 type Page = IntroPage | QuestionPage
 
 type Msg  = ShowIntro
           | ShowQuestion
           | TryAnswer String
           | GiveUp
+          | NewIndex Int
           | GenerateNextIndex
           | RetrieveQuestion
 
@@ -38,55 +51,57 @@ initQuestion = Q
 pickQuestion : List (List String) -> Int -> Q
 pickQuestion qs ind =
   case head <| drop ind qs of
-    Just [q, a] -> Q q a False
+    Just [q, a] -> Q (toUpper q) (toLower a) False
     _ -> Q "No more! :(" "λ" False
 
 type alias Model =
   { page     : Page
   , question : Q
   , index    : Int
-  , randGen  : Random.Generator Int
   }
 
-model : Model
-model = Model
-  QuestionPage
-  initQuestion
-  0
-  (Random.int 0 <| length everyQuestion)
+init : Model
+init = Model QuestionPage initQuestion 0
 
-
-update : Msg -> Model -> Model
+-- update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     ShowIntro ->
-      { model | page = IntroPage }
+      ({ model | page = IntroPage }, Cmd.none)
     ShowQuestion ->
-      { model | page = QuestionPage }
+      ({ model | page = QuestionPage }, Cmd.none)
     RetrieveQuestion ->
       { model
         | question = pickQuestion everyQuestion (model.index + 1)
-        , index    = model.index + 1
         }
-    GenerateNextIndex -> model
+      ! []
+    GenerateNextIndex ->
+      (model, Random.generate NewIndex questionGen)
+    NewIndex ind ->
+      { model
+        | index = ind
+        , question = pickQuestion everyQuestion ind
+        }
+      ! []
     GiveUp ->
       let question = model.question
-      in { model | question = { question | revealed = True }}
+      in { model | question = { question | revealed = True }} ! []
     TryAnswer ans ->
       let question = model.question
       in if ans == model.question.answer
-        then { model | question = { question | revealed = True }}
-        else model
+        then { model | question = { question | revealed = True }} ! []
+        else (model, Cmd.none)
 
 
 questionView : Model -> Html Msg
-questionView = lazy <| Question.view TryAnswer GiveUp RetrieveQuestion
+questionView = lazy <| Question.view TryAnswer GiveUp GenerateNextIndex
 
 navigation : Nav.Navigation Msg
 navigation =
   [ ( "Home"     , ShowIntro    )
   , ( "Questions", ShowQuestion )
   ]
+
 
 view : Model -> Html Msg
 view model =
@@ -98,8 +113,9 @@ view model =
     ]
 
 main : Program Never
-main = beginnerProgram
-  { model  = model
-  , view   = view
+main = program
+  { init   = (init, Cmd.none)
   , update = update
+  , view   = view
+  , subscriptions = always Sub.none
   }
--- src/Questions/Component.elm
+++ src/Questions/Component.elm
@@ -1,27 +1,33 @@
 module Questions.Component exposing (..)
 
 import String exposing (map)
-import Html exposing (Html, div, h1, h3, h4, text, button, input)
+import Set exposing (Set, fromList, member)
+import Html exposing (Html, div, h1, h2, h4, text, button, input)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (placeholder, type')
 
-charMask c = case c of
-  ' '  -> ' '
-  '.'  -> '.'
-  ','  -> ','
-  '\'' -> '\''
-  '"'  -> '"'
-  _    -> '*'
+ignoreChars : Set Char
+ignoreChars = fromList
+  [ ' ' , '.' , ',' , '>' , '<' , '!' , '@' , '#' , '$' , '%' , '^'
+  , '&' , '*' , '(' , ')' , '[' , ']' , '{' , '}' , '-' , '+' , '='
+  , '_'
+  ]
+
+isIgnored  = flip member ignoreChars
+charMask c = if isIgnored c then c else '*'
 
 view try giveUp next model =
   let q = model.question
   in div []
-    [ h1 [] [ text q.question ]
-    , h3 [] [ if q.revealed then text q.answer else text <| map charMask q.answer ]
-    , input [ type' "text" , placeholder "answer" , onInput try ] [ ]
+    [ h4 [] [ text <| toString model.index ]
+    , h1 [] [ text q.question ]
+    , h2 [] [ if q.revealed then text q.answer else text <| map charMask q.answer ]
     , div []
       [ if q.revealed
         then button [onClick next] [ text "Next Question" ]
-        else button [onClick giveUp] [ text "Give Up" ]
+        else div []
+          [ input [ type' "text" , placeholder "answer" , onInput try ] [ ]
+          , button [onClick giveUp] [ text "Give Up" ]
+          ]
       ]
     ]
